b08adb91c58a96001141271350761e5a7583c4b0
Joao Diniz <joaopbddiniz@gmail.com>
2020-12-13 22:44:15 +0000

    adicionado ErrorBoundry para mostrar uma mensagem de erro ao cliente

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,6 +1,9 @@
 import Card from "./Card"
 
 const CardList = ({ robots }) => {
+  if (true) {
+    throw new Error('NOOOOOOO');
+  }
   const cardsArray = robots.map((robot) => {
     return <Card key={robot.id} id={robot.id} name={robot.name} email={robot.email} />
   });
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 class App extends Component {
   constructor() {
@@ -36,7 +37,9 @@ class App extends Component {
           <h1 className="f1">KittenFriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
           <Scroll>
-            <CardList robots={filteredKittens} />
+            <ErrorBoundry>
+              <CardList robots={filteredKittens} />
+            </ErrorBoundry>
           </Scroll>
         </div>
       );
